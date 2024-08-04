8814eaf52a8402ec51c1bbb317c51bd57ac9e47c
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-08-04 08:05:18 +0000

    css var

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,56 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  // import app from './main';
+  export let name;
+  let child_height = 120;
 </script>
 
 <main>
+  {name}
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
+
   <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div class="parent" style="height: --child_height;">
+    <div style='height:{child_height}%' class="child1">1</div>
+    <div class="child2">2</div>
+    <div class="hght" style='width:25%'>3</div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  </div>
 </main>
 
 <style>
+  .parent {
+    display: flex;
+    align-items: flex-end;
+    background: rgb(237, 231, 231);
+    border: 1px solid gray;
+    height: 200px;
+  }
+
+  .hght{
+   height: var(--child_height,'%');
+   background:blue;
+  }
+
+  .child1 {
+    width: 25%;
+    height: 40%;
+    background-color: beige;
+  }
+  .child2 {
+    width: 25%;
+    height: 110%;
+    background-color: rgb(141, 141, 49);
+  }
+
+
   .logo {
     height: 6em;
     padding: 1.5em;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
+  props: {'name':document.body.innerHTML}
 })
-
+console.log(app)
 export default app
